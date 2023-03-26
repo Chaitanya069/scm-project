c2d26de6e073e78cbdaf0f538ac6cb0374ffa450
Chaitanya Garg <122776359+Chaitanya069@users.noreply.github.com>
2023-03-26 11:39:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def printing_stuff():
 def new_acc_details():
     name = input("Enter account holder name : ")
     # age = int(input("Enter your age : "))
-    aadhar_no = input("Enter your aadhar number : ")
+    aadhar_no = input("Enter your aadhar no. : ")
     acc_num = aadhar_no
     acc_nums[acc_num] = 0
     set_pin = input("Enter New Pin: ")
@@ -38,7 +38,7 @@ def checking_pin():
 
 def transferring():
     if a not in acc_nums.keys() or b not in acc_nums.keys():
-        print("Not a valid Acc. Number ")
+        print("Not a valid Acc. No. ")
         print()
 
     else:
